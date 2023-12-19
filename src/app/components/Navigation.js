c5fa69d781210c56be1fc7238f5566af5d908c6e
Kris1027 <obarzanek.work@gmail.com>
2023-12-19 15:34:36 +0100

    build main layout of the app

---
+++ src/app/components/Navigation.js
@@ -0,0 +1,17 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href='/'>Home</a>
+        </li>
+        <li>
+          <a href='/products'>Products</a>
+        </li>
+        <li>
+          <a href='/cart'>Cart</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
