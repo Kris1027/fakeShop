ac56798693fa34769e0b9f8a4259689e9b693304
Kris1027 <obarzanek.work@gmail.com>
2023-12-20 19:10:34 +0100

    full refactor

--- src/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(): React.ReactElement {
-  return (
-    <footer className="bg-light p-4 text-center text-dark dark:bg-dark dark:text-light">
-      <p>Footer</p>
-    </footer>
-  );
-}
--- src/app/components/Logo.tsx
+++
@@ -1,3 +0,0 @@
-export default function Logo() {
-  return <h1 className='text-2xl font-bold'>fakeShop</h1>;
-}
