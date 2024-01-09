3c3f06f46e3f58603a695be2ef119da21455a6a0
Kris1027 <obarzanek.work@gmail.com>
2024-01-10 00:11:43 +0100

    build dynamic route product component

--- Frontend/src/app/products/[productId]/page.tsx
+++ Frontend/src/app/products/[productId]/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { ProductProps, getProduct } from "../ProductsApi";
+import Button from "@/app/ui/Button";
+
 export default function DetailedProductPage({
   params,
 }: {
   params: { productId: string };
 }) {
+  const [product, setProduct] = useState<ProductProps | null>(null);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    getProduct(params.productId).then(setProduct);
+  }, [params.productId]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Product {params.productId}</h1>
-    </div>
+    <main>
+      <h1>{product.name}</h1>
+      <p>{product.price} $</p>
+      <img src={product.image} alt={product.name} />
+      <p>{product.description}</p>
+      <Button variant="buy" label="Back" onClick={() => router.back()} />
+      <Button variant="buy" label="Add to cart" />
+    </main>
   );
 }
